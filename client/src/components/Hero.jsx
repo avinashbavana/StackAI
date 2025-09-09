b6d87ff9cf2e5c2bcfb667baae9342cd2e13d325
B-Avinash-Projects <bavinash46989@gmail.com>
2025-09-09 23:34:14 +0530

    setup: added Clerk provider and environment variables and feat(auth): integrated Clerk with env config

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,10 @@
+
+const Hero = () => {
+  return (
+    <div className='px-4 sm:px-20 xl:px-32 relative inline-flex flex-col w-full justify-center bg-[url(/gradientBackground.png)] object-cover bg-no-repeat min-h-screen'>
+
+    </div>
+  ) 
+}
+
+export default Hero
